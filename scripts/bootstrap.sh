3c8d28f4f646f6bbbed7afc828c19049ed51692c
OmerAmsc <Omeramsc@gmail.com>
2019-11-20 22:37:03 +0200

    moved flask app to operate in background & created requirements file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 dnf -y update
 dnf -y install python3-pip
-pip3 install --user Flask
+pip3 install -r /vagrant/requirements.txt
 
 chmod 755 /vagrant/app.py
-/vagrant/app.py
+nohup python3 /vagrant/app.py > /dev/null 2>&1 &
