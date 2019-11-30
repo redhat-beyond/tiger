86d4e991ce59aaf6e7b554ea62427eecf952399e
OmerAmsc <Omeramsc@gmail.com>
2019-11-30 23:51:16 +0200

    created "contact us" page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('/contact_us.html')
 
 
 if __name__ == '__main__':
