a29f36f25e1625722935d27a6bfaaa363098a288
Barak Merdler <barakmerdler@gmail.com>
2019-11-30 17:38:30 +0200

    adding script to install mysql, change memory to 1024 for mysql can run

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-
+mv /tmp/start-sql-config.txt /etc/yum.repos.d/mysql-community.repo
 dnf -y update
+dnf -y install mysql-community-server
+systemctl enable mysqld.service
+systemctl start mysqld.service
 dnf -y install python3-pip
 pip3 install -r /vagrant/requirements.txt
-
-
 
 chmod 755 /vagrant/app.py
 nohup python3 /vagrant/app.py > /dev/null 2>&1 &
