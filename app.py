8452f047bc555750e6fdd34488b39255aced8ca1
Saar Cohen <saar935@gmail.com>
2019-12-14 21:44:42 +0200

    automate_the_create_date

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
+from datetime import date
+def get_Date():
+    return date.today()
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
-
 @app.route('/')
 def Home():
     return render_template('Home.html')
-
-
 @app.route('/contact_us')
 def contact_us():
     return render_template('/contact_us.html')
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+
