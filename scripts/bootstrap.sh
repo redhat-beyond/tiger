b5a6ac387a7e8b91c6a154ffbf5b64e9c4f89ce3
OmerAmsc <Omeramsc@gmail.com>
2019-12-15 21:35:27 +0200

    fixed bootstrap script to launch Mysql with a chosen password and create the db

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,5 +6,13 @@ systemctl enable mysqld.service
 systemctl start mysqld.service
 pip3 install -r /vagrant/requirements.txt
 
+old_pw=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+new_pw='LoginPass@@11223344'
+mysqladmin -u root -p"$old_pw" password "$new_pw"
+mysql -u root -p"$new_pw" <<MYSQL_SCRIPT
+CREATE DATABASE tiger;
+USE tiger;
+MYSQL_SCRIPT
+
 chmod 755 /vagrant/app.py
 nohup python3 /vagrant/app.py > /dev/null 2>&1 &
