0bdaf2cb7bae3ee3c9c6e3f93a8edd2cd308ccd8
roey <roey12@gmail.com>
2019-12-22 16:59:01 +0200

    add session to app.py

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
+import os
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
 
 @app.route('/')
 def Home():
-    return render_template('Home.html')
-
+	if not session.get("EMAIL") is None:
+		return render_template('Home.html' , email=session["EMAIL"])
+	else:
+		return render_template('Home.html')
 
 @app.route('/contact_us')
 def contact_us():
     return render_template('/contact_us.html')
 
+@app.route('/signup',methods=["GET", "POST"])
+def sign_up():
+	if request.method == "POST":
+		req = request.form
+		email = req.get("email")
+		password = req.get("password")
+		session["EMAIL"]=email
+		session["PASSWORD"]=password
+		print(session["EMAIL"])
+		print(session)
+		return render_template('Home.html',email=session["EMAIL"])
+	return render_template('/sign-up.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
