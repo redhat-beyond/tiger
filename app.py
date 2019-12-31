73382cd1cb382acab70e663965632baf78248d2a
Saar Cohen <saar935@gmail.com>
2019-12-31 19:10:48 +0200

    testing_1

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/templates')
 
 
-@app.route('/')
+@app.route("/")
 def Home():
     return render_template('Home.html')
 
 
-@app.route('/contact_us')
-def contact_us():
-    return render_template('/contact_us.html')
+#@app.route('/contact_us')
+#def contact_us():
+#    return render_template('/contact_us.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host="localhost")
