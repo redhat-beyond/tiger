b1821977f2085ba6ef1c5e9a0c3aa44843a73842
Nikita Bugrovsky <nbugrovs@redhat.com>
2020-01-01 15:20:44 +0200

    bringing tiger to working state

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "fedora/30-cloud-base"
   config.vm.provision "shell", path:"scripts/bootstrap.sh"
-  config.vm.network "forwarded_port", guest: 5000, host: 5000 
-  config.vm.provider "virtualbox" do |v|
-    v.memory = 1024
-  end
+  config.vm.network "forwarded_port", guest: 5000, host: 5000, host_ip: "127.0.0.1"
 end
--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='/templates')
+app = Flask(__name__)
 
 
 @app.route("/")
-def Home():
-    return render_template('Home.html')
+def home():
+    return render_template('/home.html')
 
 
-#@app.route('/contact_us')
-#def contact_us():
-#    return render_template('/contact_us.html')
+@app.route('/contact_us')
+def contact_us():
+    return render_template('/contact_us.html')
 
 
 if __name__ == '__main__':
-    app.run(host="localhost")
+    app.run(host='0.0.0.0', port=5000)
