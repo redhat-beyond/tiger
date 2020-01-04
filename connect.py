831a62a0a3aa705fb146ac82915837ebf242b8c0
mor <morbitton1993@gmail.com>
2020-01-04 23:54:29 +0200

    Added messages method

--- connect.py
+++ connect.py
@@ -4,7 +4,7 @@ def definedlog(fileHandler):
     logger.setLevel(logging.ERROR)
     handler = logging.FileHandler(fileHandler)
     handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+    formatter = logging.Formatter('% (asctime)s - % (name)s - % (levelname)s : % (message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
