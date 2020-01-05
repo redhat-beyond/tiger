9db511a6884249873226bb630284ae97d3c4c36e
shirmn <shirmn@gmail.com>
2020-01-05 11:09:02 +0200

    add function check user name

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import mysql.connector
 
-app = Flask(__name__)
+app=Flask(_name_)
+
+mydb=mysql.connector.connect(host = "localhost",user = "root",passwd = "LoginPass@@11223344",database = "tiger")
 
 
 @app.route('/')
@@ -16,3 +19,26 @@ def contact_us():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+def check_username(us):
+    maulers = mydb.cursor()
+
+    Fender = "SELECT * FROM users WHERE username  ="+ us
+
+    maulers.execute(Fender)
+
+    result = mycursor.fetchall()
+
+    if not result:
+        # the user doesnt exist
+        return false
+    else:
+        # the user exists
+        return true
+
+
+
+
+
+
+
