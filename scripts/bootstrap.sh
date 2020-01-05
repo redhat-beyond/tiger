0636260882e003fe4e6dd47ef87546a27cb24e9f
Barak Merdler <barakmerdler@gmail.com>
2020-01-05 18:00:35 +0200

    addressed changes from Liora

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,16 +10,25 @@ then
     debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password LoginPass@@11223344'
     # !----------! install python and mysql !----------!
     apt-get -y install python3-pip mysql-server mysql-client
-    pip3 install -r /vagrant/requirements.txt
+    
+
+
+else
+    echo "im fedora"
+    cp /vagrant/mysql-community.repo /etc/yum.repos.d/
+    dnf -y update
+    dnf -y install python3-pip mysql-community-server
+
+
+    old_pw=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+    new_pw='LoginPass@@11223344'
+    mysqladmin -u root -p"$old_pw" password "$new_pw"
+
+fi
+pip3 install -r /vagrant/requirements.txt
     systemctl enable mysql
     systemctl start mysql
     new_pw='LoginPass@@11223344'
-    # !----------! Allow External Connections on your MySQL Service !--------!
-    sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-    sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-    mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH privileges;"
-    service mysql restart
-    # !----------! Creating tiger !--------!
     mysql -u root -p"$new_pw" <<MYSQL_SCRIPT
     CREATE DATABASE tiger;
     USE tiger;
@@ -32,28 +41,3 @@ MYSQL_SCRIPT
    
     chmod 755 /vagrant/app.py
     nohup python3 /vagrant/app.py > /dev/null 2>&1 &
-else
-    echo "im fedora"
-    cp /vagrant/mysql-community.repo /etc/yum.repos.d/
-    dnf -y update
-    dnf -y install python3-pip mysql-community-server
-    systemctl enable mysqld.service
-    systemctl start mysqld.service
-    pip3 install -r /vagrant/requirements.txt
-
-    old_pw=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-    new_pw='LoginPass@@11223344'
-    mysqladmin -u root -p"$old_pw" password "$new_pw"
-    mysql -u root -p"$new_pw" <<MYSQL_SCRIPT
-    CREATE DATABASE tiger;
-    USE tiger;
-    CREATE TABLE users(username VARCHAR(20), password VARCHAR(100) NOT NULL, create_date TIMESTAMP NOT NULL, PRIMARY KEY(username));
-    CREATE TABLE messages(username VARCHAR(20),create_date TIMESTAMP NOT NULL,content VARCHAR(100) NOT NULL,FOREIGN KEY(username) REFERENCES users(username));
-    SHOW TABLES;
-    DESCRIBE users;
-    DESCRIBE messages;
-MYSQL_SCRIPT
-
-    chmod 755 /vagrant/app.py
-    nohup python3 /vagrant/app.py > /dev/null 2>&1 &
-fi
