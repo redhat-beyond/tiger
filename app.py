c7f2aaacad58ce7d4faa8b959f17419f83f8f7ba
roeyfredi <48213600+roeyfredi@users.noreply.github.com>
2020-01-06 18:00:29 +0200

    update session page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, session, request
-import os
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
+
 
 @app.route('/')
 def Home():
@@ -12,12 +12,14 @@ def Home():
 	else:
 		return render_template('Home.html')
 
+
 @app.route('/contact_us')
 def contact_us():
     return render_template('/contact_us.html')
 
-@app.route('/signup',methods=["GET", "POST"])
-def sign_up():
+
+@app.route('/log_in', methods=['GET', 'POST'])
+def log_in():
 	if request.method == "POST":
 		req = request.form
 		email = req.get("email")
@@ -25,7 +27,14 @@ def sign_up():
 		session["EMAIL"]=email
 		session["PASSWORD"]=password
 		return render_template('Home.html',email=session["EMAIL"])
-	return render_template('/sign-up.html')
+	return render_template('/sign_up.html')
+# TODO write logic to this function
+
+@app.route('/log_out')
+# TODO after seesion will be merge write this function
+def log_out():
+    return render_template('Home.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
