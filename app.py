26e822e0d4f293e57e12fde3202551fc33b76a99
Saar Cohen <saar935@gmail.com>
2020-01-08 13:59:52 +0200

    Only_Homepage_2

--- app.py
+++ app.py
@@ -9,10 +9,5 @@ def home():
     return render_template('/home.html')
 
 
-@app.route('/contact_us')
-def contact_us():
-    return render_template('/contact_us.html')
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
