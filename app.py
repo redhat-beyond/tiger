3551b71af4afca75e01d0621dcfd639afe34b527
roey <roey12@gmail.com>
2020-01-08 13:57:37 +0200

    fix conflict

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
 
+
 @app.route('/')
 def Home():
 	if not session.get("EMAIL") is None:
@@ -17,8 +18,9 @@ def Home():
 def contact_us():
     return render_template('/contact_us.html')
 
-@app.route('/signup',methods=["GET", "POST"])
-def sign_up():
+
+@app.route('/log_in', methods=['GET', 'POST'])
+def log_in():
 	if request.method == "POST":
 		req = request.form
 		email = req.get("email")
@@ -26,7 +28,14 @@ def sign_up():
 		session["EMAIL"]=email
 		session["PASSWORD"]=password
 		return render_template('Home.html',email=session["EMAIL"])
-	return render_template('/sign-up.html')
+	return render_template('/sign_up.html')
+# TODO write logic to this function
+
+@app.route('/log_out')
+# TODO after seesion will be merge write this function
+def log_out():
+    return render_template('Home.html')
+
 
 @app.route('/log_in', methods=['GET', 'POST'])
 def log_in():
@@ -48,4 +57,3 @@ def log_out():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
