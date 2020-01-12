cddc5c9441d9ce492412d2981b04e2fac0da2dec
Barak Merdler <barakmerdler@gmail.com>
2020-01-13 00:40:53 +0200

    fixing mysql on fedora

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env bash
 os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-echo $os
+
+set_mysql () {
+    systemctl enable $1
+    systemctl start $1
+}
+
 if [ $os == '"Ubuntu"' ] 
 then
-    echo "im ubuntu"
     apt-get update
     # !----------! Setting MySQL root user password root/root !----------!
     debconf-set-selections <<< 'mysql-server mysql-server/root_password password LoginPass@@11223344'
     debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password LoginPass@@11223344'
     # !----------! install python and mysql !----------!
     apt-get -y install python3-pip mysql-server mysql-client
-    
+    set_mysql "mysql"
 
 
 else
-    echo "im fedora"
     cp /vagrant/mysql-community.repo /etc/yum.repos.d/
     dnf -y update
     dnf -y install python3-pip mysql-community-server
-
+    set_mysql "mysqld.service"
 
     old_pw=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
     new_pw='LoginPass@@11223344'
     mysqladmin -u root -p"$old_pw" password "$new_pw"
+    
 
 fi
+
+
+
 pip3 install -r /vagrant/requirements.txt
-    systemctl enable mysql
-    systemctl start mysql
+
     new_pw='LoginPass@@11223344'
     mysql -u root -p"$new_pw" <<MYSQL_SCRIPT
     CREATE DATABASE tiger;
